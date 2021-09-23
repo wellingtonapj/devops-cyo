9a4f03d6e2b839977156effac46309521613c991
valuedriven <junilson@hotmail.com>
2021-09-23 19:53:18 -0300

    Exemplo merge

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 ### Teste2
 
+Outra tag...
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,7 +4,6 @@ function App() {
   return (
     <div className="App">
       <h1> Devops-CYO </h1>
-      <h2> Conteúdo principal</h2>
       <h2> Prova de conceito de uma nova tecnologia</h2>
       <h3> v1.1.0 </h3>
     </div>
